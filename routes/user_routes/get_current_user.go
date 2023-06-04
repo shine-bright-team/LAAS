7fd060dc5ee13c14eb0633d09807217e762de1bb
I_AM_TINE <sutichok46@gmail.com>
2023-06-04 17:23:47 +0700

    [ADD] isLender and Token to get_current_user

--- routes/user_routes/get_current_user.go
+++ routes/user_routes/get_current_user.go
@@ -2,10 +2,12 @@ package userroutes
 
 import (
 	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/shine-bright-team/LAAS/v2/db"
 	dbmodel "github.com/shine-bright-team/LAAS/v2/db/db_model"
 	globalmodels "github.com/shine-bright-team/LAAS/v2/global_models"
+	"github.com/shine-bright-team/LAAS/v2/utils"
 )
 
 func GetCurrentUser(c *fiber.Ctx) error {
@@ -18,13 +20,20 @@ func GetCurrentUser(c *fiber.Ctx) error {
 	var kyc dbmodel.Kyc
 	dbResult := db.DB.Where("user_id = ?", userId).Last(&kyc)
 
-	return c.JSON(globalmodels.UserInfoResponse{
-		Username:  user.Username,
-		Email:     user.Email,
-		Lastname:  user.Lastname,
-		Firstname: user.Firstname,
-		Id:        fmt.Sprint(user.ID),
-		Title:     string(user.Title),
-		IsKyc:     !(dbResult.Error != nil || !kyc.IsApproved),
-	})
+	if token, err := utils.SignToken(fmt.Sprint(user.ID), user.IsLender); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(globalmodels.ErrorResponse{Type: "Internal server error", Message: "We're having an issue on server, please try again later"})
+	} else {
+		return c.JSON(globalmodels.UserInfoResponse{
+			Username:  user.Username,
+			Email:     user.Email,
+			Lastname:  user.Lastname,
+			Firstname: user.Firstname,
+			Id:        fmt.Sprint(user.ID),
+			Title:     string(user.Title),
+			IsKyc:     !(dbResult.Error != nil || !kyc.IsApproved),
+			IsLender:  user.IsLender,
+			Token:     token,
+		})
+	}
+
 }
