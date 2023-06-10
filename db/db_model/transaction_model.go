7b3f0c9801d66daa385815ec295377107f6fbd20
I_AM_TINE <sutichok46@gmail.com>
2023-06-10 17:15:40 +0700

    [FIX] ErrMessage to be nullable

--- db/db_model/transaction_model.go
+++ db/db_model/transaction_model.go
@@ -12,7 +12,7 @@ type Transaction struct {
 	ID         uint      `gorm:"primary_key;autoIncrement:true"`
 	PaidAmount float64   `gorm:"not null"`
 	PaidAt     time.Time `gorm:"not null"`
-	ErrMessage string    `gorm:"default:''"`
+	ErrMessage *string   `gorm:"default:null"`
 	IsApproved bool      `gorm:"default:false"`
 	Contract   Contract  `gorm:"foreignKey:ContractId"`
 }
