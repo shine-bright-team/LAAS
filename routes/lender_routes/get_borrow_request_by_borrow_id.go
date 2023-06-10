dd7a069bbe21af464cd192b8da47f66eb2e76213
I_AM_TINE <sutichok46@gmail.com>
2023-06-10 18:33:48 +0700

    [ADD] get borrow request by borrow id

--- routes/lender_routes/get_borrow_request_by_borrow_id.go
+++ routes/lender_routes/get_borrow_request_by_borrow_id.go
@@ -1,7 +1,45 @@
 package lender_routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"errors"
+	"github.com/gofiber/fiber/v2"
+	"github.com/shine-bright-team/LAAS/v2/db"
+	dbmodel "github.com/shine-bright-team/LAAS/v2/db/db_model"
+	globalmodels "github.com/shine-bright-team/LAAS/v2/global_models"
+	"gorm.io/gorm"
+)
 
 func GetBorrowRequestByBorrowId(c *fiber.Ctx) error {
-	return c.SendString("Get Borrow Request by BorrowId")
+
+	borrowerId := c.Params("borrowId")
+	userId := c.Locals("userId").(int)
+
+	var contract dbmodel.Contract
+
+	if result := db.DB.Model(&contract).Preload("User").First(&contract, borrowerId); result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return c.Status(fiber.StatusNotFound).SendString("Contract not found")
+		}
+		return c.Status(fiber.StatusInternalServerError).SendString("There is an error from our side please try again later")
+	}
+
+	if contract.LenderUserId != uint(userId) {
+		return c.Status(fiber.StatusUnauthorized).SendString("You are not authorized to do this action")
+	}
+
+	if !contract.IsApproved {
+		return c.Status(fiber.StatusBadRequest).SendString("Contract is already approved")
+	}
+
+	return c.JSON(globalmodels.BorrowRequestResponse{
+		BorrowId:        contract.ID,
+		Username:        contract.Borrower.Username,
+		UserId:          contract.BorrowerUserId,
+		Firstname:       contract.Borrower.Firstname,
+		Lastname:        contract.Borrower.Lastname,
+		RequestedAmount: contract.LoanAmount,
+		RemainingAmount: nil,
+		RequestedAt:     contract.CreatedAt,
+		DueDate:         nil,
+	})
 }
