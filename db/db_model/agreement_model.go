db4301ba620cc40868bac3a5756d427dbf1e30d3
inerxia <erxinxiao@gmail.com>
2023-06-13 18:01:48 +0700

    Update agreement

--- db/db_model/agreement_model.go
+++ db/db_model/agreement_model.go
@@ -6,9 +6,11 @@ import (
 
 type Agreement struct {
 	gorm.Model
+	UserId       uint
 	ID           uint       `gorm:"primary_key;autoIncrement:true"`
 	InterestRate float32    `gorm:"not null"`
 	DueIn        int32      `gorm:"not null"`
 	Addition     string     `gorm:"default:''"`
 	Contracts    []Contract `gorm:"foreignKey:AgreementId"`
+	Owner        User       `gorm:"foreignKey:UserId"`
 }
