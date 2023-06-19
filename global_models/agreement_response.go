9155b68999db7bda29f7fe83417479506075a473
I_AM_TINE <sutichok46@gmail.com>
2023-06-19 23:04:09 +0700

    [FIX] change InterestRate from float to string

--- global_models/agreement_response.go
+++ global_models/agreement_response.go
@@ -2,8 +2,8 @@ package globalmodels
 
 type AgreementResponse struct {
 	UserId       uint
-	ID           uint    `gorm:"primary_key;autoIncrement:true"`
-	InterestRate float32 `gorm:"not null"`
-	DueIn        int32   `gorm:"not null"`
-	Addition     string  `gorm:"default:''"`
+	ID           uint
+	InterestRate *string
+	DueIn        int32
+	Addition     string
 }
