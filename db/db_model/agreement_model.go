1be7fac6f06aa0f1aa4c98a2458454677ea69f17
inerxia <erxinxiao@gmail.com>
2023-06-22 23:53:07 +0700

    add activeAtLeast and baseSalary

--- db/db_model/agreement_model.go
+++ db/db_model/agreement_model.go
@@ -12,6 +12,8 @@ type Agreement struct {
 	HighestAmount      float32    `gorm:"default:0"`
 	InterestRate       *float32   `gorm:"default:null"`
 	DueIn              int32      `gorm:"not null"`
+	ActiveAtLeast      int16      `gorm:"default:null"`
+	BaseSalary         int32      `gorm:"default:null"`
 	Addition           string     `gorm:"default:''"`
 	Contracts          []Contract `gorm:"foreignKey:AgreementId"`
 	IsInterestPerMonth bool       `gorm:"default:false"`
