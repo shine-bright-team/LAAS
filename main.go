f456a69ca60b92898a56b97e92cf09e4d80d8b43
sittichok Ouamsiri <sutichok46@gmail.com>
2023-06-28 10:31:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"github.com/shine-bright-team/LAAS/v2/utils"
-	"log"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/shine-bright-team/LAAS/v2/initialize"
 	"github.com/shine-bright-team/LAAS/v2/routes"
+	"github.com/shine-bright-team/LAAS/v2/utils"
+	"log"
+	"time"
 )
 
 func main() {
@@ -17,14 +16,6 @@ func main() {
 	log.Print("Connecting to DB")
 	if err := initialize.DbSetUp(); err != nil {
 		log.Fatalf("There is an error setting up Database: %s", err)
-	}
-
-	if !fiber.IsChild() {
-		for {
-			time.Sleep(time.Hour * 24)
-			log.Print("Adding Interest Rate to Load")
-			utils.AddInterestRateToLoad()
-		}
 	}
 
 	app.Use(cors.New())
@@ -38,4 +29,12 @@ func main() {
 	log.Print("Listening to port 8000")
 
 	app.Listen(":8000")
+
+	if !fiber.IsChild() {
+		for {
+			time.Sleep(time.Hour * 24)
+			log.Print("Adding Interest Rate to Load")
+			utils.AddInterestRateToLoad()
+		}
+	}
 }
